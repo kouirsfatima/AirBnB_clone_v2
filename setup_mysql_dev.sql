959783ff73cd7d6a848d2bbf0e81680adeef2e6f
kouirsfatima <fatimaberline535@gmail.com>
2023-12-20 23:03:53 +0100

    setup_mysql_

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
--- creates the database hbnb_test_db and the user hbnb_test  if not exists
--- give all privileges on the database hbnb_test_db to hbnb_test
--- give SELECT privilege on the database performance_schema to hbnb_test
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS ' hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+-- creates the database hbnb_dev_db and the user hbnb_dev if not exists
+-- give all privileges on the database hbnb_dev_db to hbnb_dev
+-- give SELECT privilege on the database performance_schema to hbnb_dev
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
