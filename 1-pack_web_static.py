4a41ae9fc699aadb6236e2b4f3592e73281e0b84
root <rozenhertfatima>
2024-01-11 10:35:15 -0800

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+from os.path import isdir 
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    file_name = "versions/web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S")
+    )
+    if not isdir("versions"):
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file_name)).failed is True:
+        return None
+    return file_name
+
