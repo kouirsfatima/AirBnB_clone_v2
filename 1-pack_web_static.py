62ffe343af87b78b11194a93fb677eb5c97c3ad3
root <rozenhertfatima>
2024-01-11 11:59:31 -0800

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,19 +4,19 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
-
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if not isdir("versions"):
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+    )
+
+    if not os.path.isdir("versions"):
         if local("mkdir -p versions").failed is True:
             return None
+
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+
     return file
+
