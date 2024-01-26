b7b1405a4f810fadb49bb7a0856a7432f64c433e
kouirsfatima <fatimaberline535@gmail.com>
2024-01-26 20:14:04 +0100

    1-hbnb_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-@app.route("/")
+
+@app.route('/')
 def hello_hbnb():
-    """print hello hbnh!"""
+    """print hello HBNB!"""
     return 'Hello HBNB!'
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
