263fc653a1d6a2a3b71a2c15cf1d9299e41ca895
kouirsfatima <fatimaberline535@gmail.com>
2024-01-26 16:46:33 +0100

    0-setup_web_static.sh

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route("/")
 def hello_hbnb():
     """print web"""
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    return 'Hello HBNB'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
