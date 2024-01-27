0e6490c5f362951bb5e3499017668070b5e68ea5
kouirsfatima <fatimaberline535@gmail.com>
2024-01-27 21:27:30 +0100

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def python_text(text='is cool'):
 @app.route('/number/<int:n>')
 def number_n(n):
     """n is a number"""
-    return f' {n} is a number'
+    return f'{n} is a number'
 
 
 if __name__ == '__main__':
