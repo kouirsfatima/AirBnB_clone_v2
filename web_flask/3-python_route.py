c58f416d2a6a70f49714310b3783753516d6eb28
kouirsfatima <fatimaberline535@gmail.com>
2024-01-27 17:25:46 +0100

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,6 +29,5 @@ def python_text(text='is cool'):
     """Print 'Python ' followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
 
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
