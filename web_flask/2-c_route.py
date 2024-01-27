010066d6d2fd49566c2da1b2c766ec03fc5aec4e
kouirsfatima <fatimaberline535@gmail.com>
2024-01-27 20:10:21 +0100

    fix error

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,4 +22,4 @@ def c_is_fun(text):
     """print C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,4 +30,4 @@ def python_text(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
