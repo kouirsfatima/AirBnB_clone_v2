285bcf244b52502d1bfe41de007f6895db0b9d7e
kouirsfatima <fatimaberline535@gmail.com>
2024-01-27 21:06:03 +0100

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Print 'Hello HBNB!'."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Print 'HBNB'."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Print 'C ' followed by the value of the text variable."""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """Print 'Python ' followed by the value of the text variable."""
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route('/number/<int:n>')
+def number_n(n):
+    """n is a number"""
+    return f' is a number{n}'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
